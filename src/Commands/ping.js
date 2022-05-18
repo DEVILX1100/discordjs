17138864858e8b291eba653f529f82c7fec7afa5
Dead Boy <87977420+DEVILX1100@users.noreply.github.com>
2022-05-18 15:09:23 +0530

    Update ping.js

--- src/Commands/ping.js
+++ src/Commands/ping.js
@@ -6,6 +6,6 @@ module.exports = {
     .setName("ping")
     .setDescription("Get the bots latency!"),
   execute: async (interaction, client) => {
-    return interaction.reply({ content: `Pong \`${client.ws.ping}ms\` 🏓` , components: [new MessageActionRow().addComponents(new MessageButton().setLabel(`${client.ws.ping}ms`).setStyle(`SECONDARY`).setDisabled(true).setCustomId(`pingg`))});
+    return interaction.reply({ content: `Pong \`${client.ws.ping}ms\` 🏓` , components: [new MessageActionRow().addComponents(new MessageButton().setLabel(`${client.ws.ping}ms`).setStyle(`SECONDARY`).setDisabled(true).setCustomId(`pingg`))]});
   },
 };
