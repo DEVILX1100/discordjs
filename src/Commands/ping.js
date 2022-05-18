d494d1cb6f3a9df05da12cf0b423f13f7ad784b8
Dead Boy <87977420+DEVILX1100@users.noreply.github.com>
2022-05-18 15:06:09 +0530

    Update ping.js

--- src/Commands/ping.js
+++ src/Commands/ping.js
@@ -1,10 +1,11 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Get the bots latency!"),
   execute: async (interaction, client) => {
-    return interaction.reply({ content: `Pong \`${client.ws.ping}ms\` 🏓` });
+    return interaction.reply({ content: `Pong \`${client.ws.ping}ms\` 🏓` , components: [new MessageActionRow().addComponents(new MessageButton().setLabel(`${client.ws.ping}ms`).setStyle(`SECONDARY`).setDisabled(true).setCustomId(`pingg`))});
   },
 };
